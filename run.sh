31e779dcf7436287d6840864959e8bd0d76dcd7b
danielz324 <daniel.zawistowski@gmail.com>
2020-04-16 08:17:56 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ if [[ "$OSTYPE" == "msys" ]]; then
 	fi
 else
 	if [ ! "$(docker ps -a | grep gridstudio)" ]; then
-		docker run --name=gridstudio --rm=false -v $PWD/grid-app:/home/source -v $PWD/grid-app/proxy/userdata:/home/userdata -p 8080:8080 -p 4430:4430 ricklamers/gridstudio:release
+		docker run --name=gridstudio --rm=false -v $PWD/grid-app:/home/source -v $PWD/grid-app/proxy/userdata:/home/userdata -p 8090:8080 -p 4490:4430 ricklamers/gridstudio:release
 	else
 		echo "gridstudio container detected - starting container - want to do a full restart? Run destroy.sh first."
 		docker start gridstudio
